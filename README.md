baa53466fd4fb936c63ca6cd2b17667a26057a88
Anderson T <tseng.andersonn@gmail.com>
2024-07-11 18:03:44 -0700

    initial commit.

---
+++ README.md
@@ -0,0 +1,10 @@
+# Server
+
+The config files for Anderson's GCP server
+
+
+Uses docker
+
+ON GCP 
+0.25 cores
+1 gb ram
---
+++ run.sh
@@ -0,0 +1,21 @@
+# 
+
+if [ ! -f ".env" ]; then
+    echo "Please create an .env file."
+    exit
+fi
+
+
+# start main docker compose
+docker compose pull
+docker compose up -d
+
+# start plausible
+# cd plausible/
+# docker compose pull
+# docker compose up -d
+# cd ..
+
+
+# follow main logs
+docker compose logs -f
